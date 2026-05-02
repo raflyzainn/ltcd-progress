d5a99ccaed4a54bf473832b27cc1e6b24981a163
Rafly Zainn <112810094+raflyzainn@users.noreply.github.com>
2026-05-02 22:06:31 +0700

    Time: 0 ms (100.00%) | Memory: 4 MB (93.92%) - LeetSync

---
+++ 27-remove-element/remove-element.go
@@ -0,0 +1,10 @@
+func removeElement(nums []int, val int) int {
+    k := 0
+    for _, v := range nums {
+        if v != val {
+            nums[k] = v
+            k++
+        }
+    }
+    return k
+}
