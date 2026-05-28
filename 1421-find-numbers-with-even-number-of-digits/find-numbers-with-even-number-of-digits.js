49a6670d0b174991f86b0982efdb05c550de61c6
Rafly Zainn <112810094+raflyzainn@users.noreply.github.com>
2026-05-28 21:50:35 +0700

    Time: 84 ms (4.60%) | Memory: 64.4 MB (5.26%) - LeetSync

---
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+    let count = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        console.log(nums[i].toString().length);
+        if (nums[i].toString().length % 2 == 0) {
+            count = count + 1;
+        }
+    }
+
+    return count
+};
