1c5e556ad93b1980eb1e6b715f5d5b9c17d60ed6
Rafly Zainn <112810094+raflyzainn@users.noreply.github.com>
2026-07-14 19:15:47 +0700

    Time: 0 ms (100.00%) | Memory: 4.1 MB (42.47%) - LeetSync

---
+++ 342-power-of-four/power-of-four.go
@@ -0,0 +1,14 @@
+func isPowerOfFour(n int) bool {
+    if n <= 0 {
+        return false
+    }
+
+    for n != 1 {
+        if n % 4 != 0 {
+            return false
+        }
+        n /= 4
+    }
+
+    return true
+}
