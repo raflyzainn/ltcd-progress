657cb9e591f9e60d5e9d222bd02c94fbce4a68fd
Rafly Zainn <112810094+raflyzainn@users.noreply.github.com>
2026-07-14 10:29:49 +0700

    Time: 0 ms (100.00%) | Memory: 3.9 MB (61.11%) - LeetSync

--- 7-reverse-integer/reverse-integer.go
+++ 7-reverse-integer/reverse-integer.go
@@ -1,9 +1,11 @@
 func reverse(x int) int {
     result := 0
+    
     for x != 0 {
         digit := x % 10
         x /= 10
         result = result*10 + digit
+
         if result > 2147483647 {
             return 0 
         } else if result < -2147483648 {
